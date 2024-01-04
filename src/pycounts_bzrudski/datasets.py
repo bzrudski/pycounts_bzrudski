bb12a3c1f837b189d5a4739b6de1dce7b472f5ad
bzrudski <benjamin.rudski@mail.mcgill.ca>
2024-01-04 12:06:57 -0500

    docs: Add deprecation notice to flatland function

--- src/pycounts_bzrudski/datasets.py
+++ src/pycounts_bzrudski/datasets.py
@@ -1,5 +1,6 @@
 from importlib import resources
 import pathlib
+import warnings
 
 def get_flatland() -> pathlib.Path:
     """Get path to example "Flatland" [1]_ text file.
@@ -13,6 +14,8 @@ def get_flatland() -> pathlib.Path:
     ----------
     .. [1] E. A. Abbott, "Flatland", Seeley & Co., 1884.
     """
+    warnings.warn("This function will be deprecated in v1.0.0.", FutureWarning)
+
     with resources.path("pycounts_bzrudski.data", "flatland.txt") as f:
         data_file_path = f
     return data_file_path
